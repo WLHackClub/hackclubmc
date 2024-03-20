c8e73e645081fd5a6c2b0d7eb459627ed17e04fa
greatericonrop <greateric@protonmail.com>
2024-03-19 22:35:17 -0400

    plugin template

---
+++ src/main/java/io/github/wlhackclub/hackclubmc/Main.java
@@ -0,0 +1,16 @@
+package io.github.wlhackclub.hackclubmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        // TODO: Write your code here
+
+        //this.getServer().getPluginManager().registerEvents(...);
+        //this.getCommand("hackclubmc").setExecutor(...);
+    }
+
+}
